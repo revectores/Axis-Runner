a884357565d745a08c173d0ab48d83ddba47300d
Revector <44167001+revectores@users.noreply.github.com>
2019-01-06 21:17:40 +0800

    Function Generation Process Update

--- src/Function.py
+++ src/Function.py
@@ -1,18 +1,25 @@
 from math import *
 from random import *
+from time import time
 
 class Function:
+    SCREEN_WIDTH = 800
+
     def __init__(self, formula, definition, formula_string, max_def, extend_v, mov_v):
         self.formula = formula               # 解析式
         self._definition = definition        # 定义域
         self.points = [Point(x, self.formula(x)) for x in definition]   # 离散点
 
-        self.formula_string = formula_string  # 解析式字符串
+        self._formula_string = formula_string  # 解析式字符串
+        self.expression = self.getExp(formula_string, definition)  # 解析式和定义域字符串
         self.max_def = max_def                # 定义域最大长度
         self.extend_v = extend_v              # 定义域延伸(变化)速度
         self.mov_v = mov_v                    # 函数移动速度矢量
-        self.max = max(self.points, key=lambda point: point.y)
-        self.min = min(self.points, key=lambda point: point.y)
+        self.expired_time = time() + (Function.SCREEN_WIDTH + self.max_def)//abs(self.extend_v)
+
+    @staticmethod
+    def getExp(formula_string, definition):
+        return
 
     @property
     def definition(self):
@@ -21,9 +28,16 @@ class Function:
     @definition.setter
     def definition(self, definition):
         self._definition = definition
+        self.expression = self.getExp(self._formula_string, self.definition)
         self.points = [Point(x, self.formula(x)) for x in definition]
-        self.max = max(self.points, key=lambda point: point.y)
-        self.min = min(self.points, key=lambda point: point.y)
+
+    @property
+    def max(self):
+        return max(self.points, key=lambda point: point.y).y
+
+    @property
+    def min(self):
+        return min(self.points, key=lambda point: point.y).y
 
 
 class FuncModel:
@@ -50,16 +64,29 @@ class FuncModel:
 
 class FuncRandom:
     @staticmethod
+    def direction():
+        return random() > 0.5
+
+    @staticmethod
     def linear(kr, br):
-        k = random(kr[0], kr[1])
-        b = random(br[0], br[1])
+        k = randint(kr[0], kr[1])
+        b = randint(br[0], br[1])
         max_def, extend_v, mov_v = 60, 20, 0
+
+        if FuncRandom.direction():
+            definition = range(-max_def, 0)
+        else:
+            extend_v = -extend_v
+            definition = range(Function.SCREEN_WIDTH, Function.SCREEN_WIDTH + max_def)
+
         formula = FuncModel.linear(k, b)
-        formula_string = "%f*x+%f" % (k, b)
-        definition = []
+        formula_string = "%d*x+%d" % (k, b)
+        # direction = FuncRandom.direction()
+        # y_0, y_w = formula(0), formula(GlobalData.WIDTH)
 
         new_function = Function(formula, definition, formula_string, max_def, extend_v, mov_v)
         return new_function
+
 
 class Point:
     def __init__(self,x,y):
@@ -72,11 +99,13 @@ class Point:
     def __sub__(self, other):
         return ((self.x - other.x)**2 + (self.y - other.y)**2)**0.5
 
+
 class UnitTest:
     def funcModelTest(self):
         fm = FuncModel()
         tf = fm.sin(1,1,0)
         print(tf(pi/2))
+
 
 if __name__ == '__main__':
     unitest = UnitTest()
