102e988acde9decad2beeb556e0ceda5bb998d29
QwQ2000 <396707050@qq.com>
2019-01-10 02:04:43 +0800

    Revert "Revert "One word.""
    This reverts commit 756acc359b3b07e8c743bbf5cb1a73aff2cd4e79.

--- src/Status.py
+++ src/Status.py
@@ -33,7 +33,7 @@ class PersonStatus(Status):
         self.hMove = 0
 
     def handle(self,event):
-        if not self.moveFlag:
+        if not self.hMove:
             if event.type == KEYDOWN:
                 if event.key == K_a:
                     self.hMove = 1
