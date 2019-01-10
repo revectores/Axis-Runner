23d0cb5fa2163b368456f7b4e8c53848e13910a6
revectores <revectores@gmail.com>
2019-01-10 14:01:49 +0800

    adjustment

--- src/Model.py
+++ src/Model.py
@@ -250,11 +250,11 @@ class AxisModel(Model):
 
     def newFunction(self):
         # power1 = FuncModel.linear([0.5, 0]) * FuncModel.power([2]) * FuncModel.linear([1, -320])
-        linear1 = FuncModel.linear([randint(-3, 3), randint(-50, 100)])
-        power2 = FuncModel.linear([0.005, randint(-1000, -300)]) * FuncModel.power([2])
-        power3 = FuncModel.linear([0.001, randint(-1000, 200)]) * FuncModel.power([2])
-        sin1 = FuncModel.linear([100, 100]) * FuncModel.sin([1]) * FuncModel.linear([0.01, randint(-100, 300)])
-        exp1 = FuncModel.linear([1, 0]) * FuncModel.exp([1.01]) * FuncModel.linear([1, randint(200, 300)])
+        linear1 = FuncRandom.adjust(FuncModel.linear([randint(-3, 3), randint(0, 1)]))
+        power2 = FuncRandom.adjust(FuncModel.linear([0.005, randint(-1000, -300)]) * FuncModel.power([2]))
+        power3 = FuncRandom.adjust(FuncModel.linear([0.001, randint(-1000, 200)]) * FuncModel.power([2]))
+        sin1 = FuncRandom.adjust(FuncModel.linear([100, 100]) * FuncModel.sin([1]) * FuncModel.linear([0.01, randint(-100, 300)]))
+        exp1 = FuncRandom.adjust(FuncModel.linear([1, 0]) * FuncModel.exp([1.01]) * FuncModel.linear([1, randint(200, 300)]))
         L = [linear1, power2, power3, sin1, exp1]
         print(exp1.expression)
         basic_fun = L[randint(0, len(L) - 1)]
