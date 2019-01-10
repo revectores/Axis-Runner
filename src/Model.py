617750756bc10d5491df97d2fb2aaa4abec66cce
revectores <revectores@gmail.com>
2019-01-10 13:33:48 +0800

    update

--- src/Model.py
+++ src/Model.py
@@ -3,21 +3,22 @@ import GlobalData
 from random import random, randint
 from math import log
 
-#Model表示一个游戏场景所需的局部数据，Model和View的关系是一种基于观察者模式的关系，View对Model进行监听，实时变化
-class Model:
-    def __init__(self,listener):
-        self.listener = listener #这里的listener就是一个View。因为目前并没有Model与多个View相关联的情况，将来如果有可以把listener换成listenerList，简单修改代码即可。
 
-    def changeData(self,type,val):
+# Model表示一个游戏场景所需的局部数据，Model和View的关系是一种基于观察者模式的关系，View对Model进行监听，实时变化
+class Model:
+    def __init__(self, listener):
+        self.listener = listener  # 这里的listener就是一个View。因为目前并没有Model与多个View相关联的情况，将来如果有可以把listener换成listenerList，简单修改代码即可。
+
+    def changeData(self, type, val):
         pass
 
 
 class ButtonResponseModel(Model):
-    def changeData(self,type,val):
+    def changeData(self, type, val):
         self.mode = type
-        self.listener.update(type,None)
-    
-    def __init__(self,listener,mode):
+        self.listener.update(type, None)
+
+    def __init__(self, listener, mode):
         super().__init__(listener)
         self.mode = mode
 
@@ -27,7 +28,7 @@ class GameModel(Model):
     SCREEN_WIDTH = 640
     g = 9.8
 
-    def __init__(self,listener, personModel, axisModel):
+    def __init__(self, listener, personModel, axisModel):
         super().__init__(listener)
         self.personModel = personModel
         self.axisModel = axisModel
@@ -38,9 +39,9 @@ class GameModel(Model):
         for function in self.axisModel.functionList:
             print(pm.left, pm.top)
             print([(point.x, point.y) for point in function.points if
-                 pm.left < point.x < pm.left + pm.width and pm.top < point.y < pm.top + pm.height])
+                   pm.left < point.x < pm.left + pm.width and pm.top < point.y < pm.top + pm.height])
             if len([point for point in function.points if
-                pm.left - 30 < point.x < pm.left + pm.width + 30 and pm.top - 50 < point.y < 30 + pm.top + pm.height]):
+                    pm.left - 30 < point.x < pm.left + pm.width + 30 and pm.top - 50 < point.y < 30 + pm.top + pm.height]):
                 GlobalData.f = 12
                 if not self.bTimeCnt:
                     self.bTimeCnt = 1
@@ -52,14 +53,14 @@ class GameModel(Model):
                 GlobalData.f = 30
                 self.bTimeCnt = 0
             return len([point for point in function.points if
-                 pm.left < point.x < pm.left + pm.width and pm.top < point.y < pm.top + pm.height])
+                        pm.left < point.x < pm.left + pm.width and pm.top < point.y < pm.top + pm.height])
 
 
 class OptionModel(ButtonResponseModel):
-    def __init__(self,listener,val):
-        super().__init__(listener,val)
-        self.sound = ['Sound:On','Sound:Off']
-        self.level = ['Level:Linear','Level:Quadratic','Level:Cubic']
+    def __init__(self, listener, val):
+        super().__init__(listener, val)
+        self.sound = ['Sound:On', 'Sound:Off']
+        self.level = ['Level:Linear', 'Level:Quadratic', 'Level:Cubic']
         self.sNum = 0
         self.lNum = 1
         self.flag = 0
@@ -67,12 +68,12 @@ class OptionModel(ButtonResponseModel):
 
 class PersonModel(Model):
     STAND_HEIGHT = 92  # 人物站立的高度
-    DOWN_HEIGHT = 60   # 人物下蹲的高度
-    WIDTH = 76         # 人物宽度
+    DOWN_HEIGHT = 60  # 人物下蹲的高度
+    WIDTH = 76  # 人物宽度
 
-    STAND_TOP = GameModel.SCREEN_HEIGHT/2 - STAND_HEIGHT   # 正常站立的人物头部边界
-    DOWN_TOP = GameModel.SCREEN_HEIGHT/2 - DOWN_HEIGHT     # 下蹲的人物头部边界
-    LEFT = GameModel.SCREEN_WIDTH/2 - WIDTH/2
+    STAND_TOP = GameModel.SCREEN_HEIGHT / 2 - STAND_HEIGHT  # 正常站立的人物头部边界
+    DOWN_TOP = GameModel.SCREEN_HEIGHT / 2 - DOWN_HEIGHT  # 下蹲的人物头部边界
+    LEFT = GameModel.SCREEN_WIDTH / 2 - WIDTH / 2
 
     max_border = {'top': 0, 'bottom': 0, 'left': LEFT, 'right': LEFT + WIDTH}
     attack_border = {'top': 150, 'bottom': -50, 'left': LEFT, 'right': LEFT + WIDTH}
@@ -93,13 +94,13 @@ class PersonModel(Model):
         self.attack_border['top'] = int(self.max_border['top'] * 1.5)
         self.attack_border['bottom'] = -int(self.max_border['bottom'] * 0.5)
 
-#    @staticmethod
-#    def real2screen_x(x):
-#        pass
+    #    @staticmethod
+    #    def real2screen_x(x):
+    #        pass
 
-#    @staticmethod
-#    def real2screen_y(y):
-#        return GameModel.SCREEN_HEIGHT - y
+    #    @staticmethod
+    #    def real2screen_y(y):
+    #        return GameModel.SCREEN_HEIGHT - y
 
     def borderUpdate(self):
         self.listener.update('border', [self.left, self.top, self.width, self.height])
@@ -131,9 +132,9 @@ class PersonModel(Model):
             self.top = self.STAND_TOP
             self.jumpStart = GlobalData.time
 
-        #self.borderUpdate()
-        self.modeUpdate() #通知view层换人物贴图。
-        #关于轮播图片实现动画的问题这个让view自己管理一个图片列表和索引就好，因为每一帧都会有update请求送过去，每update一次view换图这样就可以。人物脚下的坐标也是同样方法实现。
+        # self.borderUpdate()
+        self.modeUpdate()  # 通知view层换人物贴图。
+        # 关于轮播图片实现动画的问题这个让view自己管理一个图片列表和索引就好，因为每一帧都会有update请求送过去，每update一次view换图这样就可以。人物脚下的坐标也是同样方法实现。
 
     @property
     def lr_mode(self):
@@ -158,25 +159,25 @@ class PersonModel(Model):
 
     def getHeight(self):
         t = GlobalData.time - self.jumpStart
-        return self.v_y * t - 1/2 * GameModel.g / 2 * t * t
+        return self.v_y * t - 1 / 2 * GameModel.g / 2 * t * t
 
     def jumpUpdate(self):
         h = self.getHeight()
         if h <= 0:
             self.mode = 'walk'
             return
-        self.top = GameModel.SCREEN_HEIGHT/2 - self.height - h
-        #self.borderUpdate()
-        #if GlobalData.time - self.jumpStart >= 2 * self.v_y/GameModel.g:
+        self.top = GameModel.SCREEN_HEIGHT / 2 - self.height - h
+        # self.borderUpdate()
+        # if GlobalData.time - self.jumpStart >= 2 * self.v_y/GameModel.g:
 
     def getLR(self):
         t = GlobalData.time - self.lrStart
-        return self.v_x*log((abs(self.left - self.LEFT) + 3))
+        return self.v_x * log((abs(self.left - self.LEFT) + 3))
 
     def lrUpdate(self):
         r = self.getLR()
-        self.left = GameModel.SCREEN_WIDTH/2 - self.width/2 - (r if self.lr_mode == 'left' else -r)
-        #self.borderUpdate()
+        self.left = GameModel.SCREEN_WIDTH / 2 - self.width / 2 - (r if self.lr_mode == 'left' else -r)
+        # self.borderUpdate()
 
     def backUpdate(self):
         r = self.getLR()
@@ -231,7 +232,7 @@ class AxisModel(Model):
 
     def newLinear(self):
         kr = [-100, 100]
-        br = [-PersonModel.STAND_HEIGHT//2, PersonModel.STAND_HEIGHT]  # 限制b的范围, 保证生成的直线对玩家存在威胁
+        br = [-PersonModel.STAND_HEIGHT // 2, PersonModel.STAND_HEIGHT]  # 限制b的范围, 保证生成的直线对玩家存在威胁
         new_linear = FuncRandom.linear(kr, br)
         return new_linear
 
@@ -241,10 +242,10 @@ class AxisModel(Model):
         power2 = FuncModel.linear([0.005, randint(-1000, -300)]) * FuncModel.power([2])
         power3 = FuncModel.linear([0.001, randint(-1000, 200)]) * FuncModel.power([2])
         sin1 = FuncModel.linear([100, 100]) * FuncModel.sin([1]) * FuncModel.linear([0.01, randint(-100, 300)])
-        exp1 = FuncModel.linear([1,0]) * FuncModel.exp([1.01]) * FuncModel.linear([1,randint(200,300)])
+        exp1 = FuncModel.linear([1, 0]) * FuncModel.exp([1.01]) * FuncModel.linear([1, randint(200, 300)])
         L = [linear1, power2, power3, sin1, exp1]
         print(exp1.expression)
-        basic_fun = L[randint(0, len(L)-1)]
+        basic_fun = L[randint(0, len(L) - 1)]
         # basic_fun = FuncRandom.adjust(FuncRandom.random(3))
         formula = basic_fun.formula
         formula_string = basic_fun.expression
@@ -271,6 +272,7 @@ class UnitTest:
 if __name__ == '__main__':
     def prn_obj(obj):
         print('\n'.join(['%s: %s' % item for item in obj.__dict__.items()]))
+
 
     unitTest = UnitTest()
     unitTest.axisTest()
